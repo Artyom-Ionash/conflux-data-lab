9f519f22b2022facc63447c3d291d44b65d816de
DukeSpontaneous <eai_bat@tut.by>
2025-12-06 17:04:39 +0300

    Устаревшие типы заменил на актуальные.

--- app/components/ui/ToggleGroup.tsx
+++ app/components/ui/ToggleGroup.tsx
@@ -25,7 +25,7 @@ type ToggleGroupItemProps = React.ComponentPropsWithoutRef<typeof ToggleGroupPri
 // --- Components ---
 
 const ToggleGroup = React.forwardRef<
-  React.ElementRef<typeof ToggleGroupPrimitive.Root>,
+  HTMLDivElement,
   ToggleGroupProps
 >(({ className, children, gridCols, style, onKeyDown, ...props }, ref) => {
 
@@ -75,7 +75,7 @@ const ToggleGroup = React.forwardRef<
 ToggleGroup.displayName = ToggleGroupPrimitive.Root.displayName;
 
 const ToggleGroupItem = React.forwardRef<
-  React.ElementRef<typeof ToggleGroupPrimitive.Item>,
+  HTMLButtonElement,
   ToggleGroupItemProps
 >(({ className, children, fullWidth, ...props }, ref) => (
   <ToggleGroupPrimitive.Item
